8ee5bee53e31ba877405e05492f44492078efaf2
Dimuthu1994 <dimuthudiss6@gmail.com>
2022-01-23 09:42:39 +0530

    styling hero block

--- src/components/Pages/LandingPage.jsx
+++ src/components/Pages/LandingPage.jsx
@@ -4,6 +4,8 @@ import animationData from "../../animations/landinganimation/data";
 import { Grid } from "@mui/material";
 import { Button } from "@mui/material";
 import ButtonArrow from "../ui/5.1 ButtonArrow";
+import { Typography } from "@mui/material";
+import { LandingTypography } from "./Page.elements";
 
 export default function LandingPage() {
   const defaultOptions = {
@@ -19,13 +21,18 @@ export default function LandingPage() {
     <>
       <Grid container direction="column">
         <Grid item>
-          <Grid container direction="row">
-            <Grid item>
-              <div>
+          <Grid
+            container
+            direction="row"
+            justifyContent="flex-end"
+            alignItems="center"
+          >
+            <Grid sm item>
+              <LandingTypography align="center" variant="h2">
                 Bringing West Coast Technology
                 <br />
                 to the Midwest
-              </div>
+              </LandingTypography>
               <Grid container>
                 <Grid item>
                   <Button variant="contained">Free Estimate</Button>
@@ -38,7 +45,16 @@ export default function LandingPage() {
                 </Grid>
               </Grid>
             </Grid>
-            <Grid item>
+            <Grid
+              sm
+              item
+              style={{
+                maxWidth: "50em",
+                minWidth: "21em",
+                marginTop: "2em",
+                marginLeft: "10%",
+              }}
+            >
               <Lottie options={defaultOptions} height={"100%"} width={"100%"} />
             </Grid>
           </Grid>
--- src/components/Pages/Page.elements.jsx
+++ src/components/Pages/Page.elements.jsx
@@ -1,1 +1,14 @@
-import { Grid } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+export const LandingTypography = styled(Typography)(({ theme }) => ({
+  fontFamily: "Raleway",
+  textTransform: "none",
+  fontWeight: "700",
+  fontSize: "2.5rem",
+  color: theme.palette.primary.main,
+  lineHeight: 1.5,
+  // [theme.breakpoints.down("sm")]: {
+  //   fontSize: "0.6rem",
+  // },
+}));
